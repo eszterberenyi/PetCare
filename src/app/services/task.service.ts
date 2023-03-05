094b14aa88350844825d1add8471658db4a8fbee
eszterberenyi <e.eszterberenyi@gmail.com>
2023-03-05 11:17:46 +0100

    create task service

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+import {Task} from '../Task';
+import {Observable} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {User} from "../User";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TaskService {
+  private usersApiUrl = 'http://localhost:5000/users';
+  private tasksApiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.usersApiUrl}/?_embed=tasks`)
+  }
+
+  getTasksByUser(userId: number): Observable<User> {
+    return this.http.get<User>(`${this.usersApiUrl}/${userId}?_embed=tasks`);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.tasksApiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.tasksApiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
+  }
+
+  addTask(task: Task, userId: number): Observable<Task> {
+    task.userId = userId;
+    const url = `${this.tasksApiUrl}`
+    return this.http.post<Task>(url, task, httpOptions);
+  }
+}
